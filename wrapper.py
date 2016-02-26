5023c34dd9341ae28af918b7f2231ef8f9cad589
Jack Anteby <Janteby1@gmail.com>
2016-02-26 10:41:44 -0500

    working terminal trader

---
+++ wrapper.py
@@ -0,0 +1,12 @@
+import requests
+
+class Markit:
+	def __init__(self):
+		self.lookup_url = "http://dev.markitondemand.com/Api/v2/Lookup/json"
+		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json"
+
+	def company_search(self,string):
+		pass
+
+	def get_quote(self,string):
+		pass
